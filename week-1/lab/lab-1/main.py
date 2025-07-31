31d36eea06094331bf98e3370e7e1c458f6cd6ce
munyokii <munyoki912@gmail.com>
2025-07-31 12:50:45 +0300

    :sparkles: Successfully connected to mysql and created shop database with python

--- week-1/lab/lab-1/main.py
+++ week-1/lab/lab-1/main.py
@@ -1,17 +1,23 @@
+import os
 import mysql.connector
+
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Connect to MySQL Server
 conn = mysql.connector.connect(
     host="localhost",
-    user="", # Add user
-    password="" # Add your password
+    user="root", # Add user
+    password=os.getenv("DB_PASS") # Add your password
 )
 
 # Create a cursor object
 cursor = conn.cursor()
 
 # Create a new database
-database_name = "" # Add a unique Database name
+database_name = "shop" # Add a unique Database name
 cursor.execute(f"CREATE DATABASE IF NOT EXISTS {database_name}")
 
 print(f"Database '{database_name}' created successfully!")
